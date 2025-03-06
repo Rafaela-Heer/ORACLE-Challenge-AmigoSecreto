d631e69f4269165dec530d605ad7aa16286fd3c3
Rafaela-Heer <rafaelaheer@hotmail.com>
2025-03-06 01:05:25 -0300

    feat/friends

---
+++ amigosecreto.js
@@ -0,0 +1,41 @@
+let nomes = [];
+
+function adicionarAmigo(){
+    let inputAmigo = document.getElementById('amigo')
+    let nomeAmigo = inputAmigo.value;
+
+
+    if(!nomeAmigo) {
+        alert("Digite o nome que você quer adicionar!");
+        return;
+    }
+    nomes.push(nomeAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    atualizarLista();
+}
+
+
+function atualizarLista(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < nomes.length; i++){
+        let item = document.createElement("li");
+        item.textContent = nomes[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+
+function sortearAmigo(){
+    if(nomes.length === 0){
+        alert("Nenhum amigo adicionado.")
+        return;
+    }
+    let escolhido = nomes[Math.floor(Math.random() * nomes.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML= `O amigo sorteado foi: ${escolhido}`
+    let limparLista = document.getElementById("listaAmigos");
+    limparLista.innerHTML = "";
+}
